b709fddafe55b6a5374d05a471b7035b1f8aa59a
jairdaniel99 <160685574+jairdaniel99@users.noreply.github.com>
2024-04-17 19:27:09 -0600

    submitting form & two way binding

--- src/app/tdf/tdf.component.ts
+++ src/app/tdf/tdf.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tdf',
   templateUrl: './tdf.component.html',
-  styleUrl: './tdf.component.css'
+  styleUrl: './tdf.component.css',
 })
 export class TdfComponent {
-
+  email: string = '';
+  password: string = '';
+  gender: string = '';
+  age: number = 0;
+  onSubmit(survey: any) {
+    console.log('Email: ' + this.email);
+    console.log('Password: ' + this.password);
+    console.log('Gender: ' + this.gender);
+    console.log('Age: ' + this.age);
+  }
 }
